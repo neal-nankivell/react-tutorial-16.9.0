3184e3e34d9aa27bc5ae7453eef0033ab9119e5f
neal.nankivell <neal.nankivell@gmail.com>
2019-09-09 12:01:59 +0200

    Added simple state with toggle on to the Square

--- src/Square.tsx
+++ src/Square.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+interface SquareState {
+  value: string | null;
+}
+interface SquareProps {
+  value: number;
+}
 
-class Square extends React.Component<{ value: number }> {
+class Square extends React.Component<SquareProps, SquareState> {
+  constructor(props: SquareProps) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square" onClick={() => alert("Click!")}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
       </button>
     );
   }
