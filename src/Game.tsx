0327e2c932fe3891c637dc4944f05e89b8f2d12d
neal.nankivell <neal.nankivell@gmail.com>
2019-09-09 12:55:08 +0200

    Refactored Game into a React.Component
    In the next step we will add the history of each move from the game.
    
    This will be held on the Game state so it can no longer be
    a function component.

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Board from "./Board";
 
-const Game: React.FC = () => {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Game;
