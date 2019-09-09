9ad712138edae4e1471f932ba0b843411f4d641c
neal.nankivell <neal.nankivell@gmail.com>
2019-09-09 12:37:47 +0200

    Updated Next player message to toggle between X and O

--- src/Board.tsx
+++ src/Board.tsx
@@ -38,7 +38,7 @@ class Board extends React.Component<BoardProps, BoardState> {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
