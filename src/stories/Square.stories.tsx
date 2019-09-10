799e3050467f97c25fc1c8ee1ee46a607d3e0633
neal.nankivell <neal.nankivell@gmail.com>
2019-09-10 18:27:47 +0200

    Added the storybook knobs addon
    Implmented a knob to allow configuration of the Square

--- src/stories/Square.stories.tsx
+++ src/stories/Square.stories.tsx
@@ -3,10 +3,19 @@ import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import Square from "../Square";
+import { withKnobs, select } from "@storybook/addon-knobs/react";
 
 storiesOf("Square", module)
+  .addDecorator(withKnobs)
   .add("Empty", () => (
-    <Square value={null} onClick={action("Empty Clicked!")} />
+    <Square
+      value={select(
+        "value",
+        { Empty: null, "Player X": "X", "Player O": "O" },
+        null
+      )}
+      onClick={action("Clicked!")}
+    />
   ))
   .add("Player X", () => <Square value={"X"} onClick={action("X Clicked!")} />)
   .add("Player O", () => <Square value={"O"} onClick={action("O Clicked!")} />);
