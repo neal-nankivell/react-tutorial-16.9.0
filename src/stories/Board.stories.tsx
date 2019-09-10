da2cb06b0292fca77e9a0a8d8248240c3cf204e0
neal.nankivell <neal.nankivell@gmail.com>
2019-09-10 19:02:19 +0200

    Updated the board story knobs config to use an array

--- src/stories/Board.stories.tsx
+++ src/stories/Board.stories.tsx
@@ -3,19 +3,29 @@ import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 import Board from "../Board";
 import React from "react";
-import { withKnobs, object } from "@storybook/addon-knobs/react";
+import { withKnobs, array } from "@storybook/addon-knobs/react";
 
 storiesOf("Board", module)
   .addDecorator(withKnobs)
   .add("New game", () => (
     <Board
-      squares={object("Squares", Array(9).fill(null))}
+      squares={array("Squares", Array(9).fill(null))}
       onClick={action(`Clicked!`)}
     />
   ))
   .add("In progress game", () => (
     <Board
-      squares={["X", "O", null, null, "X", null, null, null, "O"]}
+      squares={array("Squares", [
+        "X",
+        "O",
+        null,
+        null,
+        "X",
+        null,
+        null,
+        null,
+        "O"
+      ])}
       onClick={action(`Clicked!`)}
     />
   ));
