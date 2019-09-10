2d4ef427278351f3431dd0729d1a6e4e707e1694
neal.nankivell <neal.nankivell@gmail.com>
2019-09-10 18:37:46 +0200

    Added the story book addon a11y

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
 import "../src/index.css";
+import { withA11y } from "@storybook/addon-a11y";
+
+addDecorator(withA11y);
 
 const req = require.context("../src", true, /\.stories.tsx$/);
 
