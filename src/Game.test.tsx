013d3516e19a99e68bb6cb9227685e5a84fd315f
neal.nankivell <neal.nankivell@gmail.com>
2019-09-10 10:33:39 +0200

    Updated basic game tests to use enzyme to shallow render the component

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Game from "./Game";
+import { shallow, ShallowWrapper } from "enzyme";
+import Board from "./Board";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Game />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Game Tests", () => {
+  let wrapper: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+
+  beforeAll(() => {
+    wrapper = shallow(<Game />);
+  });
+
+  it("Renders game div", () => {
+    expect(wrapper.find("game")).toBeDefined();
+  });
+
+  it("Renders game board inital state", () => {
+    const boardComponent = wrapper.find(Board);
+    expect(wrapper.find("game-board")).toBeDefined();
+    expect(boardComponent).toBeDefined();
+    expect(boardComponent.prop("squares")).toEqual(Array(9).fill(null));
+    expect(boardComponent.prop("onClick")).toBeDefined();
+    expect(boardComponent.prop("xIsNext")).toEqual(true);
+  });
+
+  it("Renders game info", () => {
+    expect(wrapper.find("game-info")).toBeDefined();
+  });
 });
