8f6773520a46f3907fc409067e4aaee7b0d79ae4
neal.nankivell <neal.nankivell@gmail.com>
2019-09-12 09:30:28 +0200

    Added some tests for the main makeMove action handling in the gameReducer

---
+++ src/reducers/gameReducer.test.ts
@@ -0,0 +1,47 @@
+import { GameState, initalAppState, BoardValue } from "../state/AppState";
+import gameReducer from "./gameReducer";
+import { makeMove } from "../actions/gameActions";
+
+const getSquares = (state: GameState) =>
+  state.history[state.stepNumber].squares;
+
+describe("gameReducer Tests", () => {
+  it("Alternates players on playMove", () => {
+    const initialState: GameState = initalAppState.game;
+
+    const sut = gameReducer;
+    const moves = [0, 1, 2, 3, 4, 8, 7, 6, 5];
+
+    let currentState = initialState;
+    for (var i = 0; i < moves.length; i++) {
+      currentState = sut(currentState, makeMove(moves[i]));
+      expect(getSquares(currentState)[moves[i]]).toEqual(
+        i % 2 == 0 ? "X" : "O"
+      );
+      expect(currentState.stepNumber).toEqual(i + 1);
+      expect(currentState.winner).toBeNull();
+      expect(currentState.history[currentState.stepNumber].xIsNext).toEqual(
+        i % 2 != 0
+      );
+    }
+  });
+});
+
+describe.each([[[0, 1, 3, 4, 6], "X"], [[4, 0, 5, 3, 1, 6], "O"]])(
+  "gameReducer Tests ",
+  (moves, winner) => {
+    it(`makeMoves(${moves}) = ${winner} Wins`, () => {
+      const sut = gameReducer;
+      let input: number[] = moves as number[];
+      let expectedOutput: BoardValue = winner as BoardValue;
+
+      let currentState = initalAppState.game;
+
+      for (var i = 0; i < input.length; i++) {
+        currentState = sut(currentState, makeMove(input[i]));
+      }
+
+      expect(currentState.winner).toEqual(expectedOutput);
+    });
+  }
+);
